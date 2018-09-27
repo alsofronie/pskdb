42457beb4e259254385f3bf5d7bbe37457d5c2d9
salboaie <master1!23>
2018-09-27 17:25:46 +0300

    wip

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 
 module.exports = {
     startDB:function(folder){
-
+        var fpds = require("./lib/FolderPersistentPDS");
+        return fpds.newPDS(folder);
     },
     parseDomainUrl:function(domainUrl){
 
